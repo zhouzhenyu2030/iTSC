62cfa1e26792afa8ecc27a5203c4d58b53779f94
tss <tss@tssmac-office.local>
2019-01-15 17:44:51 +0800

    zzz

--- iTSC/iTSC/View/Greeks/GreeksViewController.h
+++ iTSC/iTSC/View/Greeks/GreeksViewController.h
@@ -13,10 +13,39 @@
 
 @interface GreeksViewController : UIViewController
 
+
 //property
+@property (weak, nonatomic) IBOutlet UILabel *Label_RecordTime;
+//@property (weak, nonatomic) IBOutlet UILabel *Label_ItemKey;
+
+
+@property (weak, nonatomic) IBOutlet UILabel *Label_TotalDelta;
+@property (weak, nonatomic) IBOutlet UILabel *Label_TotalVega;
+@property (weak, nonatomic) IBOutlet UILabel *Label_TotalTheta;
+
+@property (weak, nonatomic) IBOutlet UILabel *Label_TotalGamma;
+@property (weak, nonatomic) IBOutlet UILabel *Label_TotalCharm;
+@property (weak, nonatomic) IBOutlet UILabel *Label_TotalVanna;
+@property (weak, nonatomic) IBOutlet UILabel *Label_TotalVolga;
+@property (weak, nonatomic) IBOutlet UILabel *Label_TotalVeta;
+@property (weak, nonatomic) IBOutlet UILabel *Label_TotalTata;
+
+
+@property (weak, nonatomic) IBOutlet UILabel *Label_TotalColor;
+@property (weak, nonatomic) IBOutlet UILabel *Label_TotalSpeed;
+@property (weak, nonatomic) IBOutlet UILabel *Label_TotalZomma;
+@property (weak, nonatomic) IBOutlet UILabel *Label_TotalUltima;
+
+
+@property (weak, nonatomic) IBOutlet UILabel *Label_TotalSRR;
+@property (weak, nonatomic) IBOutlet UILabel *Label_TotalSLR;
+@property (weak, nonatomic) IBOutlet UILabel *Label_TotalPCR;
+@property (weak, nonatomic) IBOutlet UILabel *Label_TotalCCR;
 
 
 //action
+- (IBAction)Button_Risk_Query_Click:(UIButton *)sender;
+
 
 
 @end
--- iTSC/iTSC/View/Greeks/GreeksViewController.m
+++ iTSC/iTSC/View/Greeks/GreeksViewController.m
@@ -16,6 +16,138 @@
 
 @implementation GreeksViewController
 
+@synthesize Label_RecordTime;
+//@synthesize Label_ItemKey;
 
+@synthesize Label_TotalDelta;
+@synthesize Label_TotalVega;
+@synthesize Label_TotalTheta;
+
+@synthesize Label_TotalGamma;
+@synthesize Label_TotalCharm;
+@synthesize Label_TotalVanna;
+@synthesize Label_TotalVolga;
+@synthesize Label_TotalVeta;
+@synthesize Label_TotalTata;
+
+@synthesize Label_TotalColor;
+@synthesize Label_TotalSpeed;
+@synthesize Label_TotalZomma;
+@synthesize Label_TotalUltima;
+
+@synthesize Label_TotalSRR;
+@synthesize Label_TotalSLR;
+@synthesize Label_TotalPCR;
+@synthesize Label_TotalCCR;
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+- (IBAction)Button_Risk_Query_Click:(UIButton *)sender
+{
+    
+    //
+    Label_RecordTime.text = @"-:-:-";
+    Label_TotalDelta.text = @"-";
+    Label_TotalVega.text = @"-";
+    Label_TotalTheta.text = @"-";
+    
+    Label_TotalGamma.text = @"-";
+    Label_TotalCharm.text = @"-";
+    Label_TotalVanna.text = @"-";
+    Label_TotalVolga.text = @"-";
+    Label_TotalVeta.text = @"-";
+    Label_TotalTata.text = @"-";
+    
+    Label_TotalColor.text = @"-";
+    Label_TotalSpeed.text = @"-";
+    Label_TotalZomma.text = @"-";
+    Label_TotalUltima.text = @"-";
+    
+    Label_TotalSRR.text = @"-";
+    Label_TotalSLR.text = @"-";
+    Label_TotalPCR.text = @"-";
+    Label_TotalCCR.text = @"-";
+    
+    
+    //DB Query
+    NSLog(@"GreeksViewController: start!");
+    
+    
+    [DBHelper Init];
+    
+    OHMySQLQueryContext *_queryContext=[DBHelper GetContext];
+    if(_queryContext==nil)
+    {
+        NSLog(@"GreeksViewController: Init: queryContext==nil!");
+        return;
+    }
+    
+    NSLog(@"GreeksViewController: SELECT: start!");
+    
+    
+    
+    //SELECT
+    OHMySQLQueryRequest *query = [OHMySQLQueryRequestFactory SELECT:@"runtimeinfo" condition:@"ItemKey='Risk' and EntityType='A'"];
+    NSError *error = nil;
+    NSArray *tasks = [_queryContext executeQueryRequestAndFetchResult:query error:&error];
+    
+    
+    NSUInteger count = tasks.count;
+    if(count <= 0)
+        return;
+    
+
+
+
+    
+    //显示
+    Label_RecordTime.text = [tasks objectAtIndex:count-1][@"RecordTime"];
+    
+    NSDictionary  *_field;
+    NSString* _typename;
+
+    for( int i=0; i<count; i++)
+    {
+        _field=[tasks objectAtIndex:i];
+        NSLog(@"%@", _field);
+        
+        _typename = _field[@"ItemType"];
+        
+        if([_typename isEqualToString:@"TotalDelta"]) Label_TotalDelta.text = [StringHelper sPositiveFormat:_field[@"ItemValue"] pointNumber:2];
+        if([_typename isEqualToString:@"TotalVega"]) Label_TotalVega.text = [StringHelper sPositiveFormat:_field[@"ItemValue"] pointNumber:2];
+        if([_typename isEqualToString:@"TotalTheta"]) Label_TotalTheta.text = [StringHelper sPositiveFormat:_field[@"ItemValue"] pointNumber:2];
+
+        if([_typename isEqualToString:@"TotalGamma"]) Label_TotalGamma.text = [StringHelper sPositiveFormat:_field[@"ItemValue"] pointNumber:2];
+        if([_typename isEqualToString:@"TotalCharm"]) Label_TotalCharm.text = [StringHelper sPositiveFormat:_field[@"ItemValue"] pointNumber:2];
+        if([_typename isEqualToString:@"TotalVanna"]) Label_TotalVanna.text = [StringHelper sPositiveFormat:_field[@"ItemValue"] pointNumber:2];
+        if([_typename isEqualToString:@"TotalVolga"]) Label_TotalVolga.text = [StringHelper sPositiveFormat:_field[@"ItemValue"] pointNumber:2];
+        if([_typename isEqualToString:@"TotalVeta"]) Label_TotalVeta.text = [StringHelper sPositiveFormat:_field[@"ItemValue"] pointNumber:2];
+        if([_typename isEqualToString:@"TotalTata"]) Label_TotalTata.text = [StringHelper sPositiveFormat:_field[@"ItemValue"] pointNumber:2];
+
+        if([_typename isEqualToString:@"TotalColor"]) Label_TotalColor.text = [StringHelper sPositiveFormat:_field[@"ItemValue"] pointNumber:2];
+        if([_typename isEqualToString:@"TotalSpeed"]) Label_TotalSpeed.text = [StringHelper sPositiveFormat:_field[@"ItemValue"] pointNumber:2];
+        if([_typename isEqualToString:@"TotalZomma"]) Label_TotalZomma.text = [StringHelper sPositiveFormat:_field[@"ItemValue"] pointNumber:2];
+        if([_typename isEqualToString:@"TotalUltima"]) Label_TotalUltima.text = [StringHelper sPositiveFormat:_field[@"ItemValue"] pointNumber:2];
+
+        if([_typename isEqualToString:@"TotalSRR"]) Label_TotalSRR.text = [StringHelper sPositiveFormat:_field[@"ItemValue"] pointNumber:2];
+        if([_typename isEqualToString:@"TotalSLR"]) Label_TotalSLR.text = [StringHelper sPositiveFormat:_field[@"ItemValue"] pointNumber:2];
+        if([_typename isEqualToString:@"TotalPCR"]) Label_TotalPCR.text = [StringHelper sPositiveFormat:_field[@"ItemValue"] pointNumber:2];
+        if([_typename isEqualToString:@"TotalCCR"]) Label_TotalCCR.text = [StringHelper sPositiveFormat:_field[@"ItemValue"] pointNumber:2];
+    }
+
+
+
+
+    
+    NSLog(@"GreeksViewController: SELECT: over!");
+}
 
 @end
